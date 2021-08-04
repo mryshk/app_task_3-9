10a3efee5312df45c194c31a54076b0455125ce4
mrythk <tb.kysy4rsxxxxxx@gmail.com>
2021-08-04 07:34:22 +0000

    [update]3-9_8/4

--- bookers_app/app/controllers/books_controller.rb
+++ bookers_app/app/controllers/books_controller.rb
@@ -18,6 +18,17 @@ class BooksController < ApplicationController
     @list = List.find(params[:id])
   end
 
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to book_path(list.id)
+  end
+
+  def destroy
+    list = List.find(params[:id])
+    list.destroy
+    redirect_to books_path
+  end
 
   private
     def list_params
--- bookers_app/config/routes.rb
+++ bookers_app/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'top' => 'homes#top'
-  post 'books' => 'books#create'
+  root to: 'homes#top'
   get 'books' => 'books#index'
+  post 'books' => 'books#create'
   get 'books/:id' => 'books#show',as:'book'
-  get 'books/;id/edit' => 'books#edit',as:'books_edit'
-  
+  get 'books/:id/edit' => 'books#edit',as:'edit_books'
+  patch 'books/:id' => 'books#update',as:'update_books'
+  delete 'books/:id' => 'books#destroy',as:'destroy_book'
 end
